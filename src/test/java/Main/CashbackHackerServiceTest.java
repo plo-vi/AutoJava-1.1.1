6bb7f4d9a786c00c8ab75fa87ce478e1ae5b6cdf
Viktoriia Plotnikova <viktoriia287@gmail.com>
2021-03-21 22:47:32 +0300

    junit4 created

---
+++ src/test/java/Main/CashbackHackerServiceTest.java
@@ -0,0 +1,30 @@
+package Main;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackerServiceTest {
+    CashbackHackerService service = new CashbackHackerService();
+
+    @Test
+    public void shouldAmountLessThan1000() {
+        int actual = service.remain(999);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmountMoreThan1000() {
+        int actual = service.remain(1100);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmount1000() {
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+}
