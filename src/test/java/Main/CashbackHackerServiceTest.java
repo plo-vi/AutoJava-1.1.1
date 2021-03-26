97af895b8fe6888fe50e66992f263bfc80da891b
Viktoriia Plotnikova <viktoriia287@gmail.com>
2021-03-26 16:58:36 +0300

    testng updated

--- src/test/java/Main/CashbackHackerServiceTest.java
+++ src/test/java/Main/CashbackHackerServiceTest.java
@@ -1,23 +1,23 @@
 package Main;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.AssertJUnit.assertEquals;
 
 class CashbackHackerServiceTest {
     CashbackHackerService service = new CashbackHackerService();
 
     @Test
     public void shouldAmountLessThan1000() {
-        int actual = service.remain(999);
-        int expected = 0;
+        int actual = service.remain(900);
+        int expected = 100;
         assertEquals(actual, expected);
     }
 
     @Test
     public void shouldAmountMoreThan1000() {
         int actual = service.remain(1100);
-        int expected = 100;
+        int expected = 900;
         assertEquals(actual, expected);
     }
 
